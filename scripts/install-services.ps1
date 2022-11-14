d8eab848312828e0e7b40147440759c3c62fa8f9
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 09:50:50 -0500

    add services script

---
+++ scripts/install-services.ps1
@@ -0,0 +1,4 @@
+# Disable any services that we installed
+$OpenVPNServiceName OpenVPNServiceInteractive
+Stop-Service -Name $OpenVPNServiceName
+Set-Service -Name $OpenVPNServiceName -StartupType Disabled
