b58e1c007aae5ea27699b42844e3c0bd9e56e950
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 11:47:42 -0500

    remove keepassxc startup

--- scripts/install-services.ps1
+++ scripts/install-services.ps1
@@ -2,3 +2,7 @@
 $OpenVPNServiceName = 'OpenVPNServiceInteractive'
 Stop-Service -Name $OpenVPNServiceName
 Set-Service -Name $OpenVPNServiceName -StartupType Disabled
+# Delete KeePassXC startup key
+$KeepassXC_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
+$KeepassXC_Name = 'KeePassXC'
+Remove-ItemProperty -Path $KeepassXC_Path -Name $KeepassXC_Name -Force
