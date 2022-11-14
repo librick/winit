2ff53fd98fb7941ac973f8221f9759d1ff0aaba4
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 12:17:02 -0500

    simplify script

--- scripts/install-services.ps1
+++ scripts/install-services.ps1
@@ -8,14 +8,12 @@ if ($service.Length -gt 0) {
 # Delete OpenVPN-GUI startup key
 $OpenVPN_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
 $OpenVPN_Name = 'OpenVPN-GUI'
-$OpenVPN_Exists = Get-ItemProperty $OpenVPN_Path $OpenVPN_Name -ErrorAction SilentlyContinue
-if (-NOT ($OpenVPN_Exists -eq $null) -or ($OpenVPN_Exists.Length -eq 0)) {
+if ((Get-Item -Path $OpenVPN_Path).GetValue($OpenVPN_Name) -ne $null) {
     Remove-ItemProperty -Path $OpenVPN_Path -Name $OpenVPN_Name -Force
 }
 # Delete KeePassXC startup key
 $KeepassXC_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
 $KeepassXC_Name = 'KeePassXC'
-$KeepassXC_Exists = Get-ItemProperty $KeepassXC_Path $KeepassXC_Name -ErrorAction SilentlyContinue
-if (-NOT ($KeepassXC_Exists -eq $null) -or ($KeepassXC_Exists.Length -eq 0)) {
+if ((Get-Item -Path $KeepassXC_Path).GetValue($KeepassXC_Name) -ne $null) {
     Remove-ItemProperty -Path $KeepassXC_Path -Name $KeepassXC_Name -Force
 }
