c9fb3e749a0a2a8b93495e2520102d50da28baaf
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 12:28:41 -0500

    use powershell api

--- scripts/install-services.ps1
+++ scripts/install-services.ps1
@@ -18,4 +18,7 @@ if ((Get-Item -Path $KeepassXC_Path).GetValue($KeepassXC_Name) -ne $null) {
     Remove-ItemProperty -Path $KeepassXC_Path -Name $KeepassXC_Name -Force
 }
 # Add shortcut for syncthing
-cp "C:\winit\res\syncthing.exe.lnk" "${home}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+$src = "C:\winit\res\syncthing.exe.lnk"
+$dst = "${home}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
+Copy-Item -Force $src $dst
+
