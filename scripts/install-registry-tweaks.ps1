6f897caf7c4a916bfb07b9643eb6a048dac4ea56
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 13:53:23 -0500

    move service reg tweaks

--- scripts/install-registry-tweaks.ps1
+++ scripts/install-registry-tweaks.ps1
@@ -1,45 +1,55 @@
 # Set common registry keys/values
 # See: https://devblogs.microsoft.com/powershell-community/how-to-update-or-add-a-registry-key-value-with-powershell/
 
-# Call New-ItemProperty if and only if an item at the provided path exists
 function RegPropIfItemExists() {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$true, Position=0)]
-        [string] $Path,
-        [Parameter(Mandatory=$true, Position=1)]
-        [string] $Name,
-        [Parameter(Mandatory=$true, Position=2)]
-        $Value
-    )
-    if ((Get-Item -Path $Path).GetValue($Value) -ne $null) {
-      Write-Host "wrote value ${Value} to ${Path} under ${Name}" -ForegroundColor green -BackgroundColor black
-      New-ItemProperty -Path $Path -Name $Name -Value $Value -Type Dword -Force
-    } else {
-      Write-Host "found existing ItemProperty in ${Path} under ${Name}, skipped New-ItemProperty call" -ForegroundColor yellow -BackgroundColor black
-      Set-ItemProperty -Path $Path -Name $Name -Value $Value -Type Dword -Force
-      Write-Host "wrote value ${Value} to ${Path} under ${Name}" -ForegroundColor green -BackgroundColor black
-    }
+  [CmdletBinding()]
+  param (
+      [Parameter(Mandatory=$true, Position=0)]
+      [string] $Path,
+      [Parameter(Mandatory=$true, Position=1)]
+      [string] $Name,
+      [Parameter(Mandatory=$true, Position=2)]
+      $Value
+  )
+  if ((Get-Item -Path $Path).GetValue($Value) -ne $null) {
+    Write-Host "wrote value ${Value} to ${Path} under ${Name}" -ForegroundColor green -BackgroundColor black
+    New-ItemProperty -Path $Path -Name $Name -Value $Value -Type Dword -Force
+  } else {
+    Write-Host "found existing ItemProperty in ${Path} under ${Name}, skipped New-ItemProperty call" -ForegroundColor yellow -BackgroundColor black
+    Set-ItemProperty -Path $Path -Name $Name -Value $Value -Type Dword -Force
+    Write-Host "wrote value ${Value} to ${Path} under ${Name}" -ForegroundColor green -BackgroundColor black
+  }
 }
 
-# Call New-ItemProperty and optionally New-Item if no item at the provided path exists
+function DelPropIfItemExists() {
+  [CmdletBinding()]
+  param (
+    [Parameter(Mandatory=$true, Position=0)]
+    [string] $Path,
+    [Parameter(Mandatory=$true, Position=1)]
+    [string] $Name
+  )
+  if ((Get-Item -Path $Path).GetValue($Name) -ne $null) {
+    Remove-ItemProperty -Path $Path -Name $Name -Force
+  }
+}
+
 function RegProp() {
-    [CmdletBinding()]
-    param (
-        [Parameter(Mandatory=$true, Position=0)]
-        [string] $Path,
-        [Parameter(Mandatory=$true, Position=1)]
-        [string] $Name,
-        [Parameter(Mandatory=$true, Position=2)]
-        $Value
-    )
-    If (-NOT (Test-Path $Path)) {
-      New-Item -Path $Path -Force | Out-Null
-    }
-    RegPropIfItemExists $Path $Name $Value
+  [CmdletBinding()]
+  param (
+      [Parameter(Mandatory=$true, Position=0)]
+      [string] $Path,
+      [Parameter(Mandatory=$true, Position=1)]
+      [string] $Name,
+      [Parameter(Mandatory=$true, Position=2)]
+      $Value
+  )
+  If (-NOT (Test-Path $Path)) {
+    New-Item -Path $Path -Force | Out-Null
+  }
+  RegPropIfItemExists $Path $Name $Value
 }
 
-# Call Remove-Item if and only if an item at the provided path exists
 function DelItemIfItemExists() {
   [CmdletBinding()]
   param (
@@ -64,3 +74,6 @@ RegPropIfItemExists 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Ad
 RegPropIfItemExists 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' 'TaskbarDa' 0; # Hide widgets in the taskbar
 DelItemIfItemExists 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{645FF040-5081-101B-9F08-00AA002F954E}'; # Delete recycle bin desktop shortcut
 RegPropIfItemExists 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' 'SearchboxTaskbarMode' 0; # Hide the search input in the taskbar
+# Delete startup keys
+DelPropIfItemExists 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' 'OpenVPN-GUI'
+DelPropIfItemExists 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run' 'KeePassXC'
--- scripts/install-services.ps1
+++ scripts/install-services.ps1
@@ -5,20 +5,7 @@ if ($service.Length -gt 0) {
     Stop-Service -Name $OpenVPNServiceName
     Set-Service -Name $OpenVPNServiceName -StartupType Disabled
 }
-# Delete OpenVPN-GUI startup key
-$OpenVPN_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
-$OpenVPN_Name = 'OpenVPN-GUI'
-if ((Get-Item -Path $OpenVPN_Path).GetValue($OpenVPN_Name) -ne $null) {
-    Remove-ItemProperty -Path $OpenVPN_Path -Name $OpenVPN_Name -Force
-}
-# Delete KeePassXC startup key
-$KeepassXC_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Run'
-$KeepassXC_Name = 'KeePassXC'
-if ((Get-Item -Path $KeepassXC_Path).GetValue($KeepassXC_Name) -ne $null) {
-    Remove-ItemProperty -Path $KeepassXC_Path -Name $KeepassXC_Name -Force
-}
 # Add shortcut for syncthing
 $src = "C:\winit\res\syncthing.exe.lnk"
 $dst = "${home}\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\"
 Copy-Item -Force $src $dst
-
