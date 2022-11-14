3d88adb72e28385f7d61960dd046c16f47a6236d
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 13:36:29 -0500

    add functions

--- scripts/install-registry-tweaks.ps1
+++ scripts/install-registry-tweaks.ps1
@@ -1,44 +1,57 @@
-$PersonalizePath = 'HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize'
-# Use dark theme by default
-New-ItemProperty -Path $PersonalizePath -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
-# Disable lockscreen picture (go right to login screen)
-New-ItemProperty -Path $PersonalizePath -Name NoLockScreen -Value 1 -Type Dword -Force
-
-# Show hidden files and folders
+# Set common registry keys/values
 # See: https://devblogs.microsoft.com/powershell-community/how-to-update-or-add-a-registry-key-value-with-powershell/
-# Set variables to indicate value and key to set
-$HideFileExt_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
-$HideFileExt_Name    = 'HideFileExt'
-$Hidden_Name         = 'Hidden'
-# Create the key if it does not exist
-If (-NOT (Test-Path $HideFileExt_Path)) { New-Item -Path $HideFileExt_Path -Force | Out-Null }
-New-ItemProperty -Path $HideFileExt_Path -Name $HideFileExt_Name -Value 0 -PropertyType DWORD -Force
-New-ItemProperty -Path $HideFileExt_Path -Name $Hidden_Name -Value 1 -PropertyType DWORD -Force
 
-# Disable dynamic search
-$DynamicSearch_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings'
-$DynamicSearch_Name = 'IsDynamicSearchBoxEnabled'
-New-ItemProperty -Path $DynamicSearch_Path -Name $DynamicSearch_Name -Value 0 -Type Dword -Force
-
-# Delete the recycle bin desktop shortcut
-$RecycleBin_Path = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{645FF040-5081-101B-9F08-00AA002F954E}'
-If (Test-Path $RecycleBin_Path) {
-  Remove-Item -Path $RecycleBin_Path -Recurse -Force
+# Call New-ItemProperty if and only if an item at the provided path exists
+function RegPropIfItemExists() {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $Path,
+        [Parameter(Mandatory=$true, Position=1)]
+        [string] $Name
+        [Parameter(Mandatory=$true, Position=2)]
+        $Value
+    )
+    if ((Get-Item -Path $Path).GetValue($Value) -ne $null) {
+      New-ItemProperty -Path $Path -Name $Name -Value $Value -Type Dword -Force
+    }
+}
+# Call New-ItemProperty and optionally New-Item if no item at the provided path exists
+function RegProp() {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string] $Path,
+        [Parameter(Mandatory=$true, Position=1)]
+        [string] $Name
+        [Parameter(Mandatory=$true, Position=2)]
+        $Value
+    )
+    If (-NOT (Test-Path $Path)) {
+      New-Item -Path $Path -Force | Out-Null
+    }
+    New-ItemProperty -Path $Path -Name $Name -Value $Value -PropertyType DWORD -Force
 }
 
-# Hide the task view button in the taskbar
-$HideTaskViewButton_Path = 'HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideTaskViewButton'
-$HideTaskViewButton_Name = 'value'
-New-ItemProperty -Path $HideTaskViewButton_Path -Name $HideTaskViewButton_Name -Value 1 -Type Dword -Force
-# Hide the chat/teams icon in the taskbar
-$TaskbarMn_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
-$TaskbarMn_Name = 'TaskbarMn'
-New-ItemProperty -Path $TaskbarMn_Path -Name $TaskbarMn_Name -Value 0 -Type Dword -Force
-# Hide widgets in the taskbar
-$TaskbarDa_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
-$TaskbarDa_Name = 'TaskbarDa'
-New-ItemProperty -Path $TaskbarDa_Path -Name $TaskbarDa_Name -Value 0 -Type Dword -Force
-# Hide the search input in the taskbar
-$SearchboxTaskbarMode_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search'
-$SearchbarTaskbarMode_Name = 'SearchboxTaskbarMode'
-New-ItemProperty -Path $SearchboxTaskbarMode_Path -Name $SearchbarTaskbarMode_Name -Value 0 -Type Dword -Force
+# Call Remove-Item if and only if an item at the provided path exists
+function DelItemIfItemExists() {
+  [CmdletBinding()]
+  param (
+      [Parameter(Mandatory=$true, Position=0)]
+      [string] $Path
+  )
+  If (Test-Path $Path) {
+    Remove-Item -Path $Path -Recurse -Force
+  }
+}
+
+RegPropIfItemExists('HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize', 'SystemUsesLightTheme', 0); # Force dark theme
+RegPropIfItemExists('HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize', 'NoLockScreen', 1); # Disable lock screen, go right to login
+RegProp('HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced', 'HideFileExt', 0); # Show hidden files and folders
+RegProp('HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced', 'Hidden', 1);
+RegPropIfItemExists('HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings', 'IsDynamicSearchBoxEnabled', 0); # Disable dynamic search
+RegPropIfItemExists('HKLM:\SOFTWARE\Microsoft\PolicyManager\default\Start\HideTaskViewButton', 'value', 1); # Hide the task view button in the taskbar
+RegPropIfItemExists('HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced', 'TaskbarMn', 0); # Hide the chat/teams icon in the taskbar
+RegPropIfItemExists('HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced', 'TaskbarDa', 0); # Hide widgets in the taskbar
+DelItemIfItemExists('HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Desktop\NameSpace\{645FF040-5081-101B-9F08-00AA002F954E}') # Delete recycle bin desktop shortcut
+RegPropIfItemExists('HKCU:\Software\Microsoft\Windows\CurrentVersion\Search', 'SearchboxTaskbarMode', 0); # Hide the search input in the taskbar
