81a6d3a9cc46e28d353c2bd891a2313c28a96575
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 03:04:56 -0500

    add more debloat packages

--- scripts/install-debloat-apps.ps1
+++ scripts/install-debloat-apps.ps1
@@ -1,8 +1,10 @@
 # Remove default apps from Windows 11
 Get-AppxPackage Microsoft.Clipchamp | Remove-AppxPackage
+Get-AppxPackage Clipchamp.Clipchamp | Remove-AppxPackage
 Get-AppxPackage Microsoft.BingNews | Remove-AppxPackage
 Get-AppxPackage Microsoft.BingWeather | Remove-AppxPackage
 Get-AppxPackage Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
 Get-AppxPackage Microsoft.YourPhone | Remove-AppxPackage
 Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
 Get-AppxPackage Microsoft.ZuneVideo | Remove-AppxPackage
+Get-AppxPackage Microsoft.Services.Store.Engagement | Remove-AppxPackage
