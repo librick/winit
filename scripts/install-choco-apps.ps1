0a1cd68cc5f38cb2ece71313d8035c2be7eb9f8c
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 14:31:37 -0500

    add class

--- scripts/install-choco-apps.ps1
+++ scripts/install-choco-apps.ps1
@@ -1,19 +1,46 @@
+class ChocoHelper {
+    [bool]IsInstalled() {
+        param(
+            [Parameter(Mandatory=$true, Position=0)]
+            [string] $PackageName
+        )
+        $existing = choco search -r --local-only --id-only --exact $PackageName;
+        return $existing -ne $null -and $existing.Length -gt 0;
+    }
+    LogInstalled() {
+        param(
+            [Parameter(Mandatory=$true, Position=0)]
+            [string] $PackageName
+        )
+        Write-Host "choco package ${PackageName} is already installed, skipping" `
+            -ForegroundColor green -BackgroundColor black;
+    }
+}
+
 function ChocoInstall() {
   [CmdletBinding()]
   param (
       [Parameter(Mandatory=$true, Position=0)]
       [string] $PackageName
   )
-  $existing = choco search -r --local-only --id-only --exact $PackageName
-  if ($existing -ne $null -and $existing.Length -gt 0) {
-    Write-Host "choco package ${existing} is already installed, skipping (searched for ${PackageName})" `
-        -ForegroundColor green -BackgroundColor black;
+  if ([ChocoHelper]::IsInstalled($PackageName)) {
+    [ChocoHelper]::LogInstalled($PackageName);
   } else {
     choco install -r -y $PackageName
   }
 }
+
+function InstallSysInternals() {
+    $PackageName = sysinternals
+    if ([ChocoHelper]::IsInstalled($PackageName)) {
+        [ChocoHelper]::LogInstalled($PackageName);
+    } else {
+      choco install -r -y $PackageName --params "/InstallDir:C:\Tools\sysinternals"
+    }
+}
+
 # Sysinternals
-choco install -r -y sysinternals --params "/InstallDir:C:\Tools\sysinternals"
+InstallSysInternals
 # Miscellaneous
 ChocoInstall brave
 ChocoInstall 7zip
