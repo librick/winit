b30ada08ebc7e33489934ffad998a70d7b3f5b47
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 01:32:45 -0500

    clean up registry tweaks

--- scripts/install-registry-tweaks.ps1
+++ scripts/install-registry-tweaks.ps1
@@ -7,16 +7,18 @@ New-ItemProperty -Path $PersonalizePath -Name NoLockScreen -Value 1 -Type Dword 
 # Show hidden files and folders
 # See: https://devblogs.microsoft.com/powershell-community/how-to-update-or-add-a-registry-key-value-with-powershell/
 # Set variables to indicate value and key to set
-$RegistryPath = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+$HideFileExt_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
 $HideFileExt_Name    = 'HideFileExt'
-$HideFileExt_Value   = '00000000'
 $Hidden_Name         = 'Hidden'
-$Hidden_Value        = '00000001'
 # Create the key if it does not exist
 If (-NOT (Test-Path $RegistryPath)) {
   New-Item -Path $RegistryPath -Force | Out-Null
 }
 # Now set the values
-New-ItemProperty -Path $RegistryPath -Name $HideFileExt_Name -Value $HideFileExt_Value -PropertyType DWORD -Force
-New-ItemProperty -Path $RegistryPath -Name $Hidden_Name -Value $Hidden_Value -PropertyType DWORD -Force
-New-ItemProperty -Path $PersonalizePath -Name SystemUsesLightTheme -Value 0 -Type Dword -Force
+New-ItemProperty -Path $HideFileExt_Path -Name $HideFileExt_Name -Value 0 -PropertyType DWORD -Force
+New-ItemProperty -Path $HideFileExt_Path -Name $Hidden_Name -Value 1 -PropertyType DWORD -Force
+
+# Disable dynamic search
+$DynamicSearch_Path = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\SearchSettings'
+$DynamicSearch_Name = 'IsDynamicSearchBoxEnabled'
+New-ItemProperty -Path $DynamicSearch_Path -Name $DynamicSearch_Name -Value 0 -Type Dword -Force
