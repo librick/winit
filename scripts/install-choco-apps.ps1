513edd67444c1edfc33875b438d3e72f3c809670
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 02:50:07 -0500

    remove choco install packages

--- scripts/install-choco-apps.ps1
+++ scripts/install-choco-apps.ps1
@@ -3,9 +3,7 @@ choco install -r -y sysinternals --params "/InstallDir:C:\Tools\sysinternals"
 # Miscellaneous
 choco install -r -y brave
 choco install -r -y 7zip
-choco install -r -y 7zip.install
 choco install -r -y bleachbit
-choco install -r -y bleachbit.install
 choco install -r -y blender
 choco install -r -y chocolateygui
 choco install -r -y cmder
@@ -17,9 +15,6 @@ choco install -r -y openvpn
 choco install -r -y putty
 choco install -r -y rufus
 choco install -r -y sumatrapdf
-choco install -r -y sumatrapdf.install
 choco install -r -y syncthing
 choco install -r -y vlc
-choco install -r -y vlc.install
 choco install -r -y winscp
-choco install -r -y winscp.install
