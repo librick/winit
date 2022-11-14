5c321732a5fa72332c02bfe89b02a3c9ac7b5356
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 10:56:20 -0500

    fix home dir

--- scripts/install-quick-access.ps1
+++ scripts/install-quick-access.ps1
@@ -10,7 +10,7 @@ $o = new-object -com shell.application;
 # See: https://stackoverflow.com/questions/30051634/is-it-possible-programmatically-add-folders-to-the-windows-10-quick-access-panel
 $($o.Namespace("shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}").Items() | where { $_.IsFolder -eq "True" -and ($($_.Verbs() | Where-Object {$_.Name -in "Unpin from Quick access"}) -ne $null)}).InvokeVerb("unpinfromhome")
 # Pin folders
-$o.Namespace('~').Self.InvokeVerb("pintohome");
+$o.Namespace("${home}").Self.InvokeVerb("pintohome");
 $o.Namespace('C:\ProgramData\chocolatey').Self.InvokeVerb("pintohome");
 $o.Namespace('C:\Tools\sysinternals').Self.InvokeVerb("pintohome");
 $o.Namespace('C:\syncthing').Self.InvokeVerb("pintohome");
