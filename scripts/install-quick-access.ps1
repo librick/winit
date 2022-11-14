6610e98e183a36db15c84c4801a53f1e594b771c
Eric McDonald <ericmcdonald@protonmail.com>
2022-11-14 10:50:04 -0500

    use relative paths

--- scripts/install-quick-access.ps1
+++ scripts/install-quick-access.ps1
@@ -10,12 +10,12 @@ $o = new-object -com shell.application;
 # See: https://stackoverflow.com/questions/30051634/is-it-possible-programmatically-add-folders-to-the-windows-10-quick-access-panel
 $($o.Namespace("shell:::{679f85cb-0220-4080-b29b-5540cc05aab6}").Items() | where { $_.IsFolder -eq "True" -and ($($_.Verbs() | Where-Object {$_.Name -in "Unpin from Quick access"}) -ne $null)}).InvokeVerb("unpinfromhome")
 # Pin folders
-$o.Namespace('C:\Users\user').Self.InvokeVerb("pintohome");
+$o.Namespace('~').Self.InvokeVerb("pintohome");
 $o.Namespace('C:\ProgramData\chocolatey').Self.InvokeVerb("pintohome");
 $o.Namespace('C:\Tools\sysinternals').Self.InvokeVerb("pintohome");
 $o.Namespace('C:\syncthing').Self.InvokeVerb("pintohome");
 $o.Namespace('C:\Program Files (x86)\Steam').Self.InvokeVerb("pintohome");
-$o.Namespace('C:\Users\user\Documents').Self.InvokeVerb("pintohome");
-$o.Namespace('C:\Users\user\Downloads').Self.InvokeVerb("pintohome");
-$o.Namespace('C:\Users\user\Pictures').Self.InvokeVerb("pintohome");
-$o.Namespace('C:\Users\user\Videos').Self.InvokeVerb("pintohome");
+$o.Namespace('~\Documents').Self.InvokeVerb("pintohome");
+$o.Namespace('~\Downloads').Self.InvokeVerb("pintohome");
+$o.Namespace('~\Pictures').Self.InvokeVerb("pintohome");
+$o.Namespace('~\Videos').Self.InvokeVerb("pintohome");
